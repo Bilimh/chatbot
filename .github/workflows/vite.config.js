400424e29bb20b8607c2902efcf0f102bc25c138
Bilimh <mozar2025@gmail.com>
2026-02-18 12:35:06 +0100

    responsive11

--- .github/workflows/vite.config.js
+++ .github/workflows/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/chatbot/', // Remplacez par le nom de votre dépôt
-  // ... autres configurations
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),  // chatbot
+        home: resolve(__dirname, 'home.html')    // landing page
+      }
+    }
+  }
 })
